a4ed1b9330a9020f9ba8afe3df221f1e36fdea89
Nick Baker <ndbaker1@outlook.com>
2021-11-23 05:07:56 -0600

    ssl deploy

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Connect In The Dark"
 
-const SSL = !!process.env.GATSBY_SSL
+const SSL = !!process.env.SSL
 export const environment = {
   apiDomain: process.env.API_DOMAIN || 'localhost:8000',
   ws_or_wss: SSL ? 'wss' : 'ws',
