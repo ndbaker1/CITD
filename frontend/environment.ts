4b013b43e0a9602749b912c0f0855eb48c75d854
Nick Baker <ndbaker1@outlook.com>
2021-11-23 13:00:41 -0600

    fix next public env deployment

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -1,8 +1,8 @@
 export const APP_NAME = "Connect In The Dark"
 
-const SSL = true || !!process.env.SSL
+const SSL = !!process.env.NEXT_PUBLIC_SSL
 export const environment = {
-  apiDomain: 'connect-in-the-dark.herokuapp.com' || process.env.API_DOMAIN || 'localhost:8000',
+  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN,
   ws_or_wss: SSL ? 'wss' : 'ws',
   http_or_https: SSL ? 'https' : 'http',
   healthCheck: true,
