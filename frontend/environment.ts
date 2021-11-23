63cce59dc8d202609e234823c8ee480b4ec94e6e
Nick Baker <ndbaker1@outlook.com>
2021-11-23 05:10:10 -0600

    manual deploy

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -1,8 +1,8 @@
 export const APP_NAME = "Connect In The Dark"
 
-const SSL = !!process.env.SSL
+const SSL = true || !!process.env.SSL
 export const environment = {
-  apiDomain: process.env.API_DOMAIN || 'localhost:8000',
+  apiDomain: 'connect-in-the-dark.herokuapp.com:8000' || process.env.API_DOMAIN || 'localhost:8000',
   ws_or_wss: SSL ? 'wss' : 'ws',
   http_or_https: SSL ? 'https' : 'http',
   healthCheck: true,
