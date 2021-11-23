8206022b23600ac3ef55dec7992b279e90b39775
Nick Baker <ndbaker1@outlook.com>
2021-11-23 04:58:52 -0600

    change env

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -2,7 +2,7 @@ export const APP_NAME = "Connect In The Dark"
 
 const SSL = !!process.env.GATSBY_SSL
 export const environment = {
-  apiDomain: process.env.GATSBY_API_DOMAIN || 'localhost:8000',
+  apiDomain: process.env.API_DOMAIN || 'localhost:8000',
   ws_or_wss: SSL ? 'wss' : 'ws',
   http_or_https: SSL ? 'https' : 'http',
   healthCheck: true,
